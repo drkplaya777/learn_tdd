e6bce4273e88facba1b5ba9ba2bac222ae4673d2
Shadow Walker <walkej19@gmail.com>
2018-10-27 12:46:15 -0400

    Added explict waits via polling in functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,9 +2,11 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 
 from django.test import LiveServerTestCase
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -14,12 +16,21 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
         
-    def check_for_row_in_list_table(self, row_text):
-    
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
         
-        self.assertIn(row_text, [row.text for row in rows])
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+    
+                self.assertIn(row_text, [row.text for row in rows])
+                
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
     
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Dancer has heard about a cool new online to-do app. He goes to check out 
@@ -46,20 +57,18 @@ class NewVisitorTest(LiveServerTestCase):
         # When he hits enter, the page updates, and now the page lists "1: Buy blades oil" 
         # as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         
-        self.check_for_row_in_list_table('1: Buy blades oil')
+        self.wait_for_row_in_list_table('1: Buy blades oil')
 
         # there is still a text box inviting him to add another item. he enters 
         # "Clean obsidian blades" Dancer is VERY methodical about his blades 
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Clean obsidian blades')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
         
         # The page updates again, and now shows both items on his list
-        self.check_for_row_in_list_table('1: Buy blades oil')
-        self.check_for_row_in_list_table('2: Clean obsidian blades')
+        self.wait_for_row_in_list_table('1: Buy blades oil')
+        self.wait_for_row_in_list_table('2: Clean obsidian blades')
         
         # Dancer wonders whethere the site will remember his list. Then he sees that the 
         # site generated  a unique URL for her -- There is some explantory text to that 
