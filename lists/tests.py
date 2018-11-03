ac8eaa60efa0e88f86b9854f6e1be0189a6a9d64
Shadow Walker <walkej19@gmail.com>
2018-11-03 17:05:08 -0400

    Simplified home page; created add list and view list pages

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,34 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 from lists.models import Item
 
+
+class HomePageTest(TestCase):
+    
+    def test_home_page_returns_correct_html(self):
+    
+        response = self.client.get('/')
+        
+        self.assertTemplateUsed(response, 'home.html')
+        
+
+class NewListTest(TestCase):
+    
+    def test_can_save_a_POST_request(self):
+    
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        
+        self.assertEqual(Item.objects.count(), 1, "POST request not saved")
+        
+        new_item = Item.objects.first()
+        
+        self.assertEqual(new_item.text, 'A new list item')
+        
+    def test_redirects_after_POST(self):
+        
+        response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
+        
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
@@ -21,37 +49,6 @@ class ListViewTest(TestCase):
         
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
-
-class HomePageTest(TestCase):
-    
-    def test_home_page_returns_correct_html(self):
-    
-        response = self.client.get('/')
-        
-        self.assertTemplateUsed(response, 'home.html')
-    
-    def test_can_save_a_POST_request(self):
-    
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        
-        self.assertEqual(Item.objects.count(), 1, "POST request not saved")
-        
-        new_item = Item.objects.first()
-        
-        self.assertEqual(new_item.text, 'A new list item')
-        
-    def test_redirects_after_POST(self):
-        
-        response = self.client.post('/', data={'item_text': 'A new list item'})
-        
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-            
-    def test_only_saves_items_when_necessary(self):
-    
-        self.client.get('/')
-        
-        self.assertEqual(Item.objects.count(), 0, "Item saved unnecessarily")
         
         
 class ItemModelTest(TestCase):
