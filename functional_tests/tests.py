c88b19a795259549b4ab517bfbcefeff41b5dd1c
Shadow Walker <walkej19@gmail.com>
2018-11-17 15:46:59 -0500

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -30,7 +30,36 @@ class NewVisitorTest(LiveServerTestCase):
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
-                time.sleep(0.5)
+                time.sleep(0.5) 
+                
+    def test_layout_and_styling(self):
+        # Dancer goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        
+        # He notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+            )
+        
+        # He starts a new list and sees ithe input is nicely
+        # centered there too
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        
+        self.wait_for_row_in_list_table('1: testing')
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
     
     def test_can_start_a_list_for_one_user(self):
         # Dancer has heard about a cool new online to-do app. He goes to check out 
