5b97896a7ae3629369d46f95298c8bc56b8f4dde
Shadow Walker <walkej19@gmail.com>
2018-11-25 11:24:17 -0500

    hack ALLOWED_HOSTS to be *

--- superlists/settings.py
+++ superlists/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e1la^pd&f4i$tghsw&1&7sf6s++vjd-+ro&a7u*soe6yav)!e+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
