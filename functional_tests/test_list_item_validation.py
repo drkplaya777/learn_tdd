26211b8535a6b14288062a7701438f9ff65f4801
Shadow Walker <walkej19@gmail.com>
2019-01-27 12:31:08 -0500

    Fixed JS syntax error

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,7 +7,7 @@ class ItemValidationTest(FunctionalTest):
     def get_error_element(self):
         return self.browser.find_element_by_css_selector('.has-error')
 
-    def test_error_message_are_cleared_on_input(self):
+    def test_error_messages_are_cleared_on_input(self):
         # Dancer starts a list and causes a validation error:
         self.browser.get(self.live_server_url)
         
