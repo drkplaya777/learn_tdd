818d42389a33b5c8d93c4dbb187d9835aa4f00dd
Tremorlor Denizen <walkej19@gmail.com>
2019-02-21 08:43:12 -0500

    Created view to send emails

---
+++ accounts/tests/test_views.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from unittest.mock import patch
+
+
+class SendLoginEmailViewTest(TestCase):
+
+	def test_redirects_to_home_page(self):
+	
+		# Django uses mail.outbox to ensure no email is sent
+		response = self.client.post('/accounts/send_login_email', data={
+			'email': 'karsa@house_of_chains.com'
+		})
+		
+		self.assertRedirects(response, '/')
+	
+	@patch('accounts.views.send_mail')
+	def test_sends_mail_to_address_from_post(self, mock_send_mail):
+		self.client.post('/accounts/send_login_email', data={
+			'email': 'karsa@house_of_chains.com'
+		})	
+		
+		self.assertEqual(mock_send_mail.called, True)
+		
+		(subject, body, from_mail, to_list), kwargs = mock_send_mail.call_args
+		
+		self.assertEqual(subject, 'Your login link for Superlists')
+		self.assertEqual(from_mail, 'noreply@superlists')
+		self.assertEqual(to_list, ['karsa@house_of_chains.com'])
+		
