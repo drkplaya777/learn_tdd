e167b44f5facd111345520fc4e1b1a2f5f2aea7a
Tremorlor Denizen <walkej19@gmail.com>
2019-03-31 15:33:33 -0400

    Added setting of EMAIL_PASSWORD to fabfile

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -22,7 +22,7 @@ class MyListsTest(FunctionalTest):
         self.browser.get(self.live_server_url + "/404_no_such_url/")
         self.browser.add_cookie(dict(
             name=settings.SESSION_COOKIE_NAME,
-            value=session_key,
+            value=  session_key,
             path='/',
         ))
         
