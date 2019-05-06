fda88ed65fe4a59617bb201dd5c3d65e11828141
Tremorlor Denizen <walkej19@gmail.com>
2019-05-06 08:44:28 -0400

    New FT for sharing, move session creation stuff to base

---
+++ functional_tests/test_sharing.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from .base import FunctionalTest
+
+
+def quit_if_possible(browser):
+    try:
+        browser.quit()
+    except:
+        pass
+        
+
+class SharingTest(FunctionalTest):
+
+    def test_can_share_a_list_with_another_usr(self):
+        # Dancer is a logged in user
+        self.create_pre_authenticated_session('dancer@shadow.com')
+        
+        dancer_browser = self.browser
+        self.addCleanup(lambda: quit_if_possible(dancer_browser))
+        
+        # His friend Kellanved is also hanging out on the lists site
+        kell_browser = webdriver.Firefox()
+        self.addCleanup(lambda: quit_if_possible(kell_browser))
+        
+        self.browser = kell_browser
+        self.create_pre_authenticated_session('kellanved@shadow.com')
+        
+        # Dancer goes to the home page and starts a list
+        self.browser = dancer_browser
+        self.browser.get(self.live_server_url)
+        self.add_list_item('Get help')
+        
+        # He notices a "Share this list" option
+        share_box = self.browser.find_element_by_css_selector(
+            'input[name="sharee"]'
+        )
+        
+        self.assertEqual(   
+            share_box.get_attribute('placeholder'),
+            'your-friend@example.com'
+        )
+
+        
